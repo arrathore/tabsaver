96d84cc8793a0d4970ea154ca67d0f08e79944d1
Apoorav Rathore <apoorav.rathore@icloud.com>
2024-04-29 12:47:16 -0500

    window data now persists after the window is closed

--- background.js
+++ background.js
@@ -1,21 +1,22 @@
 function tabList() {
-    // get an array of windows
-    browser.windows.getAll({populate: true}).then((windows) => {
-        windows.forEach((window, index) => {
-            // define an array to hold our urls
-            let urls = [];
-            
-            window.tabs.forEach((tab) => {
-                urls.push(tab.url);
-            });
+    // get the active window
+    browser.windows.getCurrent({populate: true}).then((window) => {
+        
+        // define an array to hold our urls
+        let urls = [];
 
-            // write each url to local storage
-            browser.storage.local.set({ ["window_" + index]: urls}).then(() => {
-                console.log("urls saved for window", index, ": ", urls);
-            }).catch((error) => {
-                console.error("error saving urls for window", index, "to local storage: ", error);
-            });  
+        window.tabs.forEach((tab) => {
+            urls.push(tab.url);
         });
+        
+        let key = "window_" + window.id;
+        // write each url to local storage
+        browser.storage.local.set({ [key]: urls}).then(() => {
+            console.log("urls saved for", key, ": ", urls);
+        }).catch((error) => {
+            console.error("error saving urls for", key, "to local storage: ", error);
+        });  
+        
     });
 }
 
