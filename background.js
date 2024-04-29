96255aa173f6d04ed4214fe399d9bffa1b3713f9
Apoorav Rathore <apoorav.rathore@icloud.com>
2024-04-29 13:54:23 -0500

    removed unused code from background script

--- background.js
+++ background.js
@@ -1,24 +1,5 @@
-/*function tabList() {
-    // get the active window
-    browser.windows.getCurrent({populate: true}).then((window) => {
-        
-        // define an array to hold our urls
-        let urls = [];
-
-        window.tabs.forEach((tab) => {
-            urls.push(tab.url);
-        });
-        
-        let key = "window_" + window.id;
-        // write each url to local storage
-        browser.storage.local.set({ [key]: urls}).then(() => {
-            console.log("urls saved for", key, ": ", urls);
-        }).catch((error) => {
-            console.error("error saving urls for", key, "to local storage: ", error);
-        });  
-        
-    });
-}
-
-browser.action.onClicked.addListener(tabList);
+/*
+kept for debugging purposes
+firefox debugger won't let me look at local storage unless the extension is actively running
+if i click away from the popup, the extension isn't considered active anymore and therefore the storage goes away
 */
