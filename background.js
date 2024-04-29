28d719245887869c8fff457f324a166b7301eb7b
Apoorav Rathore <apoorav.rathore@icloud.com>
2024-04-29 12:14:14 -0500

    added support for multiple windows

--- background.js
+++ background.js
@@ -1,24 +1,22 @@
 function tabList() {
-    
-    // get an array of tabs
-    browser.tabs.query({currentWindow: true}).then((tabs) => {
-        // define an array to hold our urls
-        let urls = [];
-        
-        tabs.forEach((tab) => {
-            urls.push(tab.url);
+    // get an array of windows
+    browser.windows.getAll({populate: true}).then((windows) => {
+        windows.forEach((window, index) => {
+            // define an array to hold our urls
+            let urls = [];
+            
+            window.tabs.forEach((tab) => {
+                urls.push(tab.url);
+            });
+
+            // write each url to local storage
+            browser.storage.local.set({ ["window_" + index]: urls}).then(() => {
+                console.log("urls saved for window", index, ": ", urls);
+            }).catch((error) => {
+                console.error("error saving urls for window", index, "to local storage: ", error);
+            });  
         });
-        
-        // write each url to local storage
-        browser.storage.local.set({urls: urls}).then(() => {
-            console.log("urls saved: ", urls);
-        }).catch((error) => {
-            console.error("error saving to local storage: ", error);
-        });
-        
-    }).catch((error) => {
-        console.error("error during query:", error);
-    })
+    });
 }
 
 browser.action.onClicked.addListener(tabList);
